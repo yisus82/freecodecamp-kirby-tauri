a71b8721655a5cf3dc882b0fc6aa5ad01db2634a
yisus82 <djalma8@gmail.com>
2024-11-18 16:43:09 +0100

    Add background image

--- src/main.js
+++ src/main.js
@@ -1,24 +1,28 @@
 import kaplay from 'kaplay';
 
+const SCALE_FACTOR = 4;
+
 const k = kaplay({
   width: 1280,
   height: 720,
   letterbox: true,
   global: false,
-  scale: 2,
+  scale: SCALE_FACTOR,
 });
 
-k.loadSprite('background', './background.png');
-k.loadSprite('clouds', './clouds.png');
-k.loadSprite('kirby', './kirby.png');
-k.loadSprite('obstacles', './obstacles.png');
+k.loadSprite('background', './images/background.png');
+k.loadSprite('clouds', './images/clouds.png');
+k.loadSprite('kirby', './images/kirby.png');
+k.loadSprite('obstacles', './images/obstacles.png');
 
-k.loadSound('confirm', './confirm.wav');
-k.loadSound('hurt', './hurt.wav');
-k.loadSound('jump', './jump.wav');
+k.loadSound('confirm', './images/confirm.wav');
+k.loadSound('hurt', './images/hurt.wav');
+k.loadSound('jump', './images/jump.wav');
 
 k.scene('start', () => {
   k.add([k.rect(k.width(), k.height()), k.color(k.Color.fromHex('#d7f2f7')), k.fixed()]);
+
+  const map = k.add([k.sprite('background'), k.pos(0, 0), k.scale(SCALE_FACTOR)]);
 });
 
 k.scene('main', () => {});
