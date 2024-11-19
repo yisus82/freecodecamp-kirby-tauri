c7a32ea88c0e5b9b466ebef4f25af1d07629cbb5
yisus82 <djalma8@gmail.com>
2024-11-19 10:51:26 +0100

    Add Play Button

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,26 @@
+// Screen
 export const WINDOW_WIDTH = 1280;
 export const WINDOW_HEIGHT = 720;
 export const SCALE_FACTOR = 4;
-export const BACKGROUND_COLOR = '#d7f2f7';
+
+// Clouds
 export const CLOUDS_SPEED = 5;
 export const CLOUDS_MAX_POS_X = 700;
 export const CLOUDS_MIN_POS_X = -500;
+
+// Player
 export const PLAYER_JUMP_FORCE = 600;
 export const PLAYER_SPEED = 600;
+
+// Colors
+export const BACKGROUND_COLOR = '#d7f2f7';
+export const BUTTON_COLOR = '#14638e';
+
+// Play button
+export const PLAY_BUTTON_WIDTH = 200;
+export const PLAY_BUTTON_HEIGHT = 50;
+export const PLAY_BUTTON_RADIUS = 3;
+export const PLAY_BUTTON_OFFSET_X = 30;
+export const PLAY_BUTTON_OFFSET_Y = 60;
+export const PLAY_BUTTON_FONT_SIZE = 24;
+export const PLAY_BUTTON_TEXT = 'Play';
--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import kaplay from 'kaplay';
 import {
   BACKGROUND_COLOR,
+  BUTTON_COLOR,
   CLOUDS_MAX_POS_X,
   CLOUDS_MIN_POS_X,
   CLOUDS_SPEED,
+  PLAY_BUTTON_FONT_SIZE,
+  PLAY_BUTTON_HEIGHT,
+  PLAY_BUTTON_OFFSET_X,
+  PLAY_BUTTON_OFFSET_Y,
+  PLAY_BUTTON_RADIUS,
+  PLAY_BUTTON_TEXT,
+  PLAY_BUTTON_WIDTH,
   SCALE_FACTOR,
   WINDOW_HEIGHT,
   WINDOW_WIDTH,
@@ -23,9 +31,9 @@ k.loadSprite('clouds', './images/clouds.png');
 k.loadSprite('kirby', './images/kirby.png');
 k.loadSprite('obstacles', './images/obstacles.png');
 
-k.loadSound('confirm', './images/confirm.wav');
-k.loadSound('hurt', './images/hurt.wav');
-k.loadSound('jump', './images/jump.wav');
+k.loadSound('confirm', './sounds/confirm.wav');
+k.loadSound('hurt', './sounds/hurt.wav');
+k.loadSound('jump', './sounds/jump.wav');
 
 k.scene('start', () => {
   k.add([k.rect(k.width(), k.height()), k.color(k.Color.fromHex(BACKGROUND_COLOR)), k.fixed()]);
@@ -45,6 +53,30 @@ k.scene('start', () => {
 
   const player = k.add(makePlayer(k));
   player.pos = k.center();
+
+  const playButton = k.add([
+    k.rect(PLAY_BUTTON_WIDTH, PLAY_BUTTON_HEIGHT, { radius: PLAY_BUTTON_RADIUS }),
+    k.color(k.Color.fromHex(BUTTON_COLOR)),
+    k.area(),
+    k.anchor('center'),
+    k.pos(k.center().x + PLAY_BUTTON_OFFSET_X, k.center().y + PLAY_BUTTON_OFFSET_Y),
+  ]);
+
+  playButton.add([
+    k.text(PLAY_BUTTON_TEXT, { size: PLAY_BUTTON_FONT_SIZE }),
+    k.color(k.Color.fromHex(BACKGROUND_COLOR)),
+    k.area(),
+    k.anchor('center'),
+  ]);
+
+  const goToGame = () => {
+    k.play('confirm');
+    k.go('main');
+  };
+
+  playButton.onClick(goToGame);
+  k.onKeyPress('space', goToGame);
+  k.onGamepadButtonPress('south', goToGame);
 });
 
 k.scene('main', () => {});
